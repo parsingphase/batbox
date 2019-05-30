efe09cddf78365a7b70d680d83f4b165f84ff1c4
Richard George <richard@phase.org>
2019-05-30 19:09:31 +0100

    Process KML

--- tracemap/views.py
+++ tracemap/views.py
@@ -3,13 +3,31 @@ from batbox import settings
 from django.http import HttpResponse
 from django.template import loader
 from os import listdir, path
+import xmltodict
+
+
+class Point:
+    name = None
+    description = None
+    style = None
+    lat = None
+    lon = None
+
+    def __repr__(self):
+        return repr(self.as_dict())
+
+    def as_dict(self):
+        return {
+            'name': self.name,
+            'description': self.description,
+            'style': self.description,
+            'position': (self.lat, self.lon)
+        }
 
 
 # Create your views here.
 def index(request):
     session_name = request.GET.get('session', '')
-
-    template = loader.get_template('tracemap/index.html')
     sessions_dir = settings.MEDIA_ROOT + 'sessions'
     sessions = [d for d in listdir(sessions_dir) if path.isdir(sessions_dir + '/' + d)]
 
@@ -32,9 +50,41 @@ def index(request):
 
     # return HttpResponse(repr(data_files))
 
+    map_data = {'points': []}
+
+    if map_files:
+        map_file = session_dir + '/' + map_files[0]
+        with open(map_file) as fd:
+            kml = xmltodict.parse(fd.read())['kml']['Document']
+            # map_data['styles'] = kml['Style']
+            # Two types of placemark: containing Point or LineString
+            points = [xml_dict_to_point(p) for p in kml['Placemark'] if 'Point' in p]
+            map_data['points'] = [p.as_dict() for p in points]
+            map_data['bounds'] = bound_from_points(points)
+
     context = {
         'session_path': session_path,
         'session_name': session_name,
-        'audio': audio_files
+        'audio': audio_files,
+        'map_data': map_data,
     }
+
+    template = loader.get_template('tracemap/index.html')
     return HttpResponse(template.render(context, request))
+
+
+def xml_dict_to_point(data: dict) -> Point:
+    point = Point()
+    point.description = data['description']
+    point.name = data['name']
+    point.style = data['styleUrl']
+    (point.lon, point.lat, _altitude) = data['Point']['coordinates'].split(',')
+    return point
+
+
+def bound_from_points(points: list):
+    min_lat = min([p.lat for p in points])
+    min_lon = min([p.lon for p in points])
+    max_lat = max([p.lat for p in points])
+    max_lon = max([p.lon for p in points])
+    return ((min_lat, min_lon), (max_lat, max_lon))
