4a8e264b218cce0caae9b7234f5db0587a7e0ba3
Richard George <richard@phase.org>
2019-06-01 18:35:36 +0100

    Improve table layout of AudioRecording admin

--- tracemap/admin.py
+++ tracemap/admin.py
@@ -3,4 +3,9 @@ from django.contrib import admin
 # Register your models here.
 from .models import AudioRecording
 
-admin.site.register(AudioRecording)
+
+class AudioRecordingAdmin(admin.ModelAdmin):
+    list_display = ('identifier', 'processed', 'recorded_at', 'latitude', 'longitude', 'genus', 'species')
+
+
+admin.site.register(AudioRecording, AudioRecordingAdmin)
