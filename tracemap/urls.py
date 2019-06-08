57cd5ba26dd4879a6247ea49d250b5508f05803f
Richard George <richard@phase.org>
2019-06-08 22:14:00 +0100

    Fix code layout

--- tracemap/urls.py
+++ tracemap/urls.py
@@ -8,5 +8,9 @@ urlpatterns = [
     path('list', views.list_view, name='list_view'),
     path('recording/<int:pk>', views.single_view, name='single_view'),
     path('genus/<str:genus>', views.genus_view, name=' genus_view'),
-    path('species/<str:genus>.<str:species>', views.species_view, name=' species_view'),
+    path(
+        'species/<str:genus>.<str:species>',
+        views.species_view,
+        name=' species_view'
+    ),
 ]
